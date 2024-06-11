5e2be753273242bda398bcf5d9916a62cabb7ab5
Sahil Yousaf <160120573+sahilyousaf2@users.noreply.github.com>
2024-06-11 12:50:40 +0000

    datatypes

---
+++ class01/02_data_types.js
@@ -0,0 +1,32 @@
+"user strict"; // treat all JS code as newer version
+
+// alert(3 + 3); //we are using nodejs, not browser
+
+console.log(3
+    + 3); // readability should he high
+
+
+console.log("sahil");
+
+
+
+let name = "Sahil";
+let age = 21;
+let logedIn = true;
+let state;
+
+
+//numbers => 2 to 53
+//bigint 
+//sritng => ""
+//boolean => true / false 
+//null => standalong value
+//undefined =>
+//symbol => unique
+
+
+//object 
+
+console.log(typeof name); // string;
+console.log(typeof null); // object 
+console.log(typeof state);// undefined
