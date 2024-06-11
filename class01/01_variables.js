25fb3b95e57bdfcc787f31aaf3f4347ac88eb6f3
Sahil Yousaf <160120573+sahilyousaf2@users.noreply.github.com>
2024-06-11 12:28:47 +0000

    Variables Done

---
+++ class01/01_variables.js
@@ -0,0 +1,22 @@
+const accountID = 1443355;
+let accuntEmail = "sahil@gmail,.com";
+var accountPass = "1234";
+accountCity = "karachi";
+
+
+// accountID =2; not allowed
+
+accuntEmail = "yousaf@gamil.com";
+accountPass = "009988";
+accountCity = "lahore"
+let accountState;
+
+// console.log(accountID);
+
+
+/*
+perfer to not var
+beacuse of issue in block scope and functional scope
+*/
+
+console.table([accountID,accuntEmail,accountPass,accountID,accountState])
