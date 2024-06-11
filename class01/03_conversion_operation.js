3254619f55c66da39a9f39c29e6e4248c684ca5e
Sahil Yousaf <160120573+sahilyousaf2@users.noreply.github.com>
2024-06-11 13:10:56 +0000

    conversion of datatypes

---
+++ class01/03_conversion_operation.js
@@ -0,0 +1,28 @@
+let score = false;
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNuber = Number(score);
+console.log(typeof valueInNuber);
+console.log(valueInNuber);
+
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+
+
+let inLoggedIn = 1;
+let isBooleanValue = Boolean(inLoggedIn);
+console.log(isBooleanValue);
+
+
+//1 => true; 0 => false
+//"" => false; "sahil" => true
+
+let someNumber = 21;
+let srtingNumber = String(someNumber);
+console.log(srtingNumber);
+console.log(typeof srtingNumber);
